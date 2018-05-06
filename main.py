a0c2341375f959e35d1fb86e6951e2091e602f30
takjsuzuki <stak.ksw@gmail.com>
2018-05-06 15:28:00 +0900

    modify

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
 import Wiener
 import OrnsteinUhlenbeck
-#import KPZ    
-
-
+#import KPZ 
 
 if __name__ == "__main__":
 
     #Model = Wiener.Wiener()
     Model = OrnsteinUhlenbeck.OrnsteinUhlenbeck()
-    #Model = KPZ.KPZ()
 
     Model.Initialize()
-    #Model.Solve()
     Model.Solve()
     Model.StatisticalQuantities()
     Model.Display()
